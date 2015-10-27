c00732190e53a8ab0126342db7fb3c07a61406e0
larry white <larz@larzwerks.com>
2015-10-27 01:45:30 -0400

    testing again line

--- example_script.sh
+++ example_script.sh
@@ -7,6 +7,7 @@ ASCII=`cat ascii/homer.txt`
 
 echo 'tesing...'
 echo 'tesing...'
+echo 'tesing again...'
 
 echo -e "\033[33mCongratulations!\033[0m You've successfully run your first build on Buildkite! 👍
 
