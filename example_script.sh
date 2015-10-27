1521482e625f3a17724e8828bfdf177c00ea2b7f
larry white <larz@larzwerks.com>
2015-10-27 02:22:45 -0400

    add yet another line

--- example_script.sh
+++ example_script.sh
@@ -9,6 +9,7 @@ echo 'testing...'
 echo 'testing...'
 echo 'testing again...'
 echo 'testing again...'
+echo 'testing again...'
 
 echo -e "\033[33mCongratulations!\033[0m You've successfully run your first build on Buildkite! 👍
 
