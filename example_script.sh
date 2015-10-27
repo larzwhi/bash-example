7f6f331c6d7ffcb952f33f286c4c66d61d0b171d
larry white <larz@larzwerks.com>
2015-10-27 01:13:08 -0400

    add testing to example_script

--- example_script.sh
+++ example_script.sh
@@ -5,6 +5,8 @@ set -e
 
 ASCII=`cat ascii/homer.txt`
 
+echo 'tesing...'
+
 echo -e "\033[33mCongratulations!\033[0m You've successfully run your first build on Buildkite! 👍
 
 \033[33m$ASCII\033[0m
